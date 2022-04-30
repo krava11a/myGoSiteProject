eeedd7f2fabc2b2b11d332e5212e06b9825d1af1
krava <krava@root.cpi>
2022-04-30 17:07:41 +0300

    Add a bit html

--- app/controller/users.go
+++ app/controller/users.go
@@ -1,11 +1,11 @@
 package controller
 
 import (
-	"encoding/json"
 	"github.com/julienschmidt/httprouter"
+	"html/template"
 	"myGoSiteProject/app/model"
 	"net/http"
-
+	"path/filepath"
 )
 
 func GetUsers(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
@@ -14,10 +14,17 @@ func GetUsers(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		http.Error(rw,err.Error(),400)
 		return
 	}
-
-	err = json.NewEncoder(rw).Encode(users)
+	main := filepath.Join("public","html","usersDynamicPage.html")
+	common :=filepath.Join("public","html","common.html")
+	tmpl, err := template.ParseFiles(main,common)
 	if err!=nil{
 		http.Error(rw,err.Error(),400)
 		return
 	}
+	err = tmpl.ExecuteTemplate(rw,"users",users)
+	if err!=nil{
+		http.Error(rw,err.Error(),400)
+		return
+	}
+
 }
